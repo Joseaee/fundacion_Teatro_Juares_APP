0985953dc8a3b8a9f2056ad9dc6df59c56a7de29
Manuel Gonzalez <gonzalezmanuel270402@gmail.com>
2024-04-30 11:33:36 -0400

    Fix: Add Provider react redux

--- App.js
+++ App.js
@@ -1,13 +1,17 @@
 import { StatusBar } from 'react-native';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import Routes from './src/routes';
+import {store} from './src/store'
+import { Provider } from 'react-redux';
 
 export default function App() {
   
   return (
+    <Provider store={store}>
     <SafeAreaProvider>  
         <StatusBar backgroundColor="#000"/>
         <Routes/>
     </SafeAreaProvider>
+    </Provider>
   );
 }
