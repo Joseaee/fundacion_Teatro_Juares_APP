bddd72f5f2f916172684a4ae70c0904c192b02b1
Manuel Gonzalez <gonzalezmanuel270402@gmail.com>
2024-05-27 12:40:20 -0400

    Fix: Change to default initial state

--- src/store/boleteria/slice.js
+++ src/store/boleteria/slice.js
@@ -6,19 +6,79 @@ const initialState = {
           id: '1',
           nombre: 'Eventito',
           categoria: 'Obra Teatral',
-          poster: require('../../../assets/img/Servicios/obras.jpg')
+          poster: require('../../../assets/img/Servicios/obras.jpg'),
+          funciones: [
+            {
+                id: '1',
+                horaInicio: '8:00 AM',
+                horaFinal: '10:00 AM',
+                fecha: '2024-05-16'
+            },
+            {
+                id: '2',
+                horaInicio: '10:00 AM',
+                horaFinal: '11:00 AM',
+                fecha: '2024-05-17'
+            },
+            {
+                id: '3',
+                horaInicio: '8:00 AM',
+                horaFinal: '9:00 AM',
+                fecha: '2024-05-18'
+            }
+        ]
         },
         {
           id: '2',
           nombre: 'Eventito 2',
           categoria: 'Concierto',
-          poster: require('../../../assets/img/Servicios/conciertos.jpg')
+          poster: require('../../../assets/img/Servicios/conciertos.jpg'),
+          funciones: [
+            {
+                id: '1',
+                horaInicio: '8:00 AM',
+                horaFinal: '10:00 AM',
+                fecha: '2024-05-16'
+            },
+            {
+                id: '2',
+                horaInicio: '10:00 AM',
+                horaFinal: '11:00 AM',
+                fecha: '2024-05-17'
+            },
+            {
+                id: '3',
+                horaInicio: '8:00 AM',
+                horaFinal: '9:00 AM',
+                fecha: '2024-05-18'
+            }
+        ]
         },
         {
           id: '3',
           nombre: 'Evento 3',
           categoria: 'Concierto',
-          poster: require('../../../assets/img/Servicios/belleza.jpg')
+          poster: require('../../../assets/img/Servicios/belleza.jpg'),
+          funciones: [
+            {
+                id: '1',
+                horaInicio: '8:00 AM',
+                horaFinal: '10:00 AM',
+                fecha: '2024-05-16'
+            },
+            {
+                id: '2',
+                horaInicio: '10:00 AM',
+                horaFinal: '11:00 AM',
+                fecha: '2024-05-17'
+            },
+            {
+                id: '3',
+                horaInicio: '8:00 AM',
+                horaFinal: '9:00 AM',
+                fecha: '2024-05-18'
+            }
+        ]
         }
       ],
       filtros: {
