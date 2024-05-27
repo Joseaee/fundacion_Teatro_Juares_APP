a0a11439dfd2801617f7bc4a7191e502075c41a1
Manuel Gonzalez <gonzalezmanuel270402@gmail.com>
2024-05-27 12:38:27 -0400

    Fix: Add styles size to styletext

--- src/components/StyleText/index.jsx
+++ src/components/StyleText/index.jsx
@@ -1,23 +1,32 @@
 import { Text, StyleSheet, View } from "react-native";
 
-export default function StyleText({children, tag, style}){
+export default function StyleText({children, tag, size, style}){
+    const fontSize = (size) ? size : 'medium'
     return (
         <View style={[{flexDirection: 'row'}, style]}>
-            <Text style={styles.text}>
+            <Text style={[styles.text, styles[fontSize]]}>
                 {children} 
             </Text>
-            <Text style={[styles.text, styles.tag, {marginStart: 4}]}>{tag}</Text>
+            <Text style={[styles.text, styles.tag, styles[fontSize] ,{marginStart: 4}]}>{tag}</Text>
         </View>
     )
 }
 
 const styles = StyleSheet.create({
     text: {
-        fontSize: 20,
         fontWeight: 'bold',
         color: '#2f2f2f',
     },
     tag: {
         color: '#E31734',
+    },
+    big:{
+        fontSize: 22,
+    },
+    medium:{
+        fontSize: 18,
+    },
+    small:{
+        fontSize: 16,
     }
 })
