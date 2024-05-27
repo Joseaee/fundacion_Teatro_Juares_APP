103debd82a35de0906ec252d156208fccda91d34
Manuel Gonzalez <gonzalezmanuel270402@gmail.com>
2024-05-27 12:41:34 -0400

    Fix: Add flatlist to functions and use selector getEventById

--- src/views/boleteria/detallesEvento/index.jsx
+++ src/views/boleteria/detallesEvento/index.jsx
@@ -1,31 +1,44 @@
-import { View, StyleSheet, Text, Image } from "react-native";
+import { View, StyleSheet, Text, Image, FlatList } from "react-native";
 import { SafeAreaView } from 'react-native-safe-area-context';
 import BottomNavbar from "../../../components/bottomNavbar";
 import Banner from "../../../components/Banner";
 import CalendarItem from "../../../components/CalendarItem";
 import StyleText from "../../../components/StyleText";
-import { useRoute } from "@react-navigation/native";
+import { useRoute, useNavigation } from "@react-navigation/native";
 import { useAppSelector } from "../../../hooks/store";
+import { getEventById } from "../../../store/selectors";
 
 export default function DetallesEvento(){
     const route = useRoute();
+    const navigation = useNavigation();
 
     const {id} = route.params
 
-    const evento = useAppSelector(state=> state.boleteria.eventos.filter(event => event.id === id))
+    const evento = useAppSelector(state=> getEventById(state, {eventId: id}))
+
+    const FuncionesItem = ({item})=>{
+        return <CalendarItem title={evento.nombre} date={item.fecha} onPress={()=> navigation.navigate('CarritoCompra', {
+            idFuncion: item.id,
+            evento: evento.nombre,
+            fecha: item.fecha,
+            horaInicio: item.horaInicio,
+            horaFinal: item.horaFinal
+        })} >
+        <Text>{item.horaInicio} - {item.horaFinal}</Text>
+    </CalendarItem>
+    }
 
     return (
         <SafeAreaView style={{flex:1}}>
             <Banner goBack={true} image={require('.:/../../assets/img/banner-cartelera.jpg')}>
-                <Image source={evento[0].poster} style={{width:140, height:140, borderRadius: 100}}/>
+                <Image source={evento.poster} style={{width:140, height:140, borderRadius: 100}}/>
             </Banner>
             <View style={{flex:1, marginTop: 30, padding:8}}>
-                <Text style={styles.title}>{evento[0].nombre}</Text>
+                <Text style={styles.title}>{evento.nombre}</Text>
                 <Text style={styles.text}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut labore eveniet ex natus odit fugiat ipsum iure odio, modi earum perferendis omnis optio itaque fugit, aperiam quaerat fuga dolore excepturi?</Text>
                 <StyleText tag='Disponibles' style={{marginVertical: 14, justifyContent: 'center'}}>Funciones</StyleText>
-                <CalendarItem title={'El eventito'} date={'2024-04-16 00:00:00'} >
-                    <Text>8:00 AM - 9:00 AM</Text>
-                </CalendarItem>
+                
+                <FlatList data={evento.funciones} renderItem={FuncionesItem} keyExtractor={item=> item.id} />
             </View>
             <BottomNavbar title={ 'Cartelera' }
                 loggedIn={ true }
@@ -44,6 +57,7 @@ const styles = StyleSheet.create({
     },
     text:{
         fontSize: 15,
-        textAlign: 'justify'
+        textAlign: 'justify',
+        color: '#2f2f2f'
     }
 })
