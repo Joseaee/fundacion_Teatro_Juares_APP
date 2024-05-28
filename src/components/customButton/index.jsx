4bb3cc4dadba497345a4be56e2c4034823d61c98
Manuel Gonzalez <gonzalezmanuel270402@gmail.com>
2024-05-28 16:52:35 -0400

    Fix: Add prop to change size of customButton

--- src/components/customButton/index.jsx
+++ src/components/customButton/index.jsx
@@ -2,13 +2,12 @@ import { Text, StyleSheet, TouchableOpacity } from 'react-native';
 import { widthPercentageToDP as wp, heightPercentageToDP as hp }from 'react-native-responsive-screen';
 import { useNavigation } from '@react-navigation/native'
 
-function CustomButton( {screen, text, params, onPress} ) {
-    
+function CustomButton( {screen, text, params, onPress, width, height} ) {
     const navigation = useNavigation();
 
     return(
         <TouchableOpacity
-            style={ styles.button }
+            style={ [styles.button, (width) ? {width} : {flex:1}, (height) ? {height} : {height: hp('6%')}] }
             onClick = { (e) => {e.stopPropagation(); onPress ? onPress() : navigation.navigate(screen, params);} }
             onPress = { (e) => {e.stopPropagation(); onPress ? onPress() : navigation.navigate(screen, params);} }
         >
@@ -18,11 +17,8 @@ function CustomButton( {screen, text, params, onPress} ) {
 }
 
 const styles = StyleSheet.create({
-
     button:{
         backgroundColor: '#CB2139',
-        height: hp('6%'),
-        flex: 1,
         justifyContent: 'center',
         alignItems: 'center',
         borderRadius: 20
@@ -30,7 +26,7 @@ const styles = StyleSheet.create({
     text:{
         color: '#fdfdfd',
         fontWeight: '700',
-        fontSize: hp('2.5%')
+        fontSize: 18
     },
 });
 
