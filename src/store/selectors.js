f26a400567f55da7f4df8b606e0b36af05daeaa4
Manuel Gonzalez <gonzalezmanuel270402@gmail.com>
2024-05-28 16:54:20 -0400

    Fix: Add selector getTotalPrize

--- src/store/selectors.js
+++ src/store/selectors.js
@@ -2,6 +2,7 @@ import { createSelector } from 'reselect';
 
 const getEvents = (state) => state.boleteria.eventos;
 const getEventId = (_, props) => props.eventId;
+const getTickets = (state)=> state.boleteria.boletos
 
 export const getEventById = createSelector(
   [getEvents, getEventId],
@@ -15,5 +16,20 @@ export const getEventFunctios = createSelector(
     (events, eventId) => {
         const event = events.find((event) => event.id === eventId);
         return event.funciones
+  }
+)
+
+export const getTotalPrize = createSelector(
+  [getTickets],
+  (tickets)=>{
+    let total = 0
+    for(const item of tickets){
+      if(item.cantidad > 1){
+        total += parseFloat(item.precio) * item.cantidad
+      }else{
+        total += parseFloat(item.precio)
+      }
+    }
+    return total.toFixed(2)
   }
 )
