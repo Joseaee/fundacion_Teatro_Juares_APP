883ade049b761c0e05d9969a0630576f8d8c8d04
Manuel Gonzalez <gonzalezmanuel270402@gmail.com>
2024-06-12 23:24:19 -0400

    Feat: Add view SeleccionarAsientos

--- src/views/boleteria/seleccionarAsientos/index.jsx
+++ src/views/boleteria/seleccionarAsientos/index.jsx
@@ -1,14 +1,22 @@
 import {Text, View, StyleSheet} from 'react-native'
 import SeatingChart from '../../../components/SeatingChart'
 import BottomNavbar from '../../../components/bottomNavbar'
+import Navbar from '../../../components/navbar'
+import StyleText from '../../../components/StyleText'
+import CustomButton from '../../../components/customButton'
 
 
 export default function SeleccionarAsientos(){
     return (
         <View style={{flex: 1}}>
+            <Navbar title='' transparent={true} back={true} backArrowColor='#E31734'/>
+            <View style={{justifyContent: 'center', alignItems: 'center'}}>
+            <StyleText tag='Area' size='big'>Seleccionar</StyleText>
+            </View>
             <View style={{flex: 1}}>
                 <SeatingChart/>
             </View>
+            <CustomButton text='Siguiente!' screen='FormasPago'></CustomButton>
             <BottomNavbar title={"Cartelera"} loggedIn={true} active={3} />
         </View>
     )
