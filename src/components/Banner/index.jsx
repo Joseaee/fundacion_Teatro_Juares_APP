cdf4b488b378a2181e74c53c6ddd556f0ae0bcb0
Manuel Gonzalez <gonzalezmanuel270402@gmail.com>
2024-06-13 12:05:16 -0400

    Agregado estilos de darkOverlay

--- src/components/Banner/index.jsx
+++ src/components/Banner/index.jsx
@@ -2,13 +2,14 @@ import {Text, ImageBackground, View, StyleSheet} from 'react-native'
 
 import Navbar from '../navbar'
 
-export default function Banner({children, image, goBack}){
+export default function Banner({children, image, goBack, darkOverlay = false}){
     return (
         <View style={{height: 160}}>
               <ImageBackground
                 source={image} 
-                style={styles.backgroundImage}
+                style={[styles.backgroundImage]}
               >
+                {(darkOverlay) ?  <View style={styles.darkOverlay}/> : null}
                 <View style={{flexDirection: 'row'}}>
                 <Navbar title='' loggedIn={true} transparent={true} back={goBack}/>
                 </View>
@@ -26,4 +27,8 @@ const styles = StyleSheet.create({
         resizeMode: 'cover',
         alignItems: 'center',
       },
+      darkOverlay: {
+        ...StyleSheet.absoluteFillObject,
+        backgroundColor: 'rgba(0,0,0,0.4)'
+    },
 })
