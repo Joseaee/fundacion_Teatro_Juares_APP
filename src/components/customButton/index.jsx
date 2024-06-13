93ef12fbbfb3a360fd8184aea851eb35f3291806
Manuel Gonzalez <gonzalezmanuel270402@gmail.com>
2024-06-12 23:19:17 -0400

    Fix: Add props to customized button

--- src/components/customButton/index.jsx
+++ src/components/customButton/index.jsx
@@ -2,29 +2,27 @@ import { Text, StyleSheet, TouchableOpacity } from 'react-native';
 import { widthPercentageToDP as wp, heightPercentageToDP as hp }from 'react-native-responsive-screen';
 import { useNavigation } from '@react-navigation/native'
 
-function CustomButton( {screen, text, params, onPress, width, height} ) {
+function CustomButton( {screen, text, params, onPress, width, height, color = '#CB2139', textColor = '#fdfdfd'} ) {
     const navigation = useNavigation();
 
     return(
         <TouchableOpacity
-            style={ [styles.button, (width) ? {width} : {flex:1}, (height) ? {height} : {height: hp('6%')}] }
+            style={ [styles.button, {backgroundColor: color}, (width) ? {width} : {flex:1}, (height) ? {height} : {height: hp('6%')}] }
             onClick = { (e) => {e.stopPropagation(); onPress ? onPress() : navigation.navigate(screen, params);} }
             onPress = { (e) => {e.stopPropagation(); onPress ? onPress() : navigation.navigate(screen, params);} }
         >
-            <Text style={ styles.text }>{ text }</Text>
+            <Text style={ [styles.text, {color: textColor}] }>{ text }</Text>
         </TouchableOpacity>
     );
 }
 
 const styles = StyleSheet.create({
     button:{
-        backgroundColor: '#CB2139',
         justifyContent: 'center',
         alignItems: 'center',
         borderRadius: 20
     },
     text:{
-        color: '#fdfdfd',
         fontWeight: '700',
         fontSize: 18
     },
