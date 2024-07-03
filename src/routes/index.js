345faeccfeb1038dfc8c920d782642a626820eef
Manuel Gonzalez <gonzalezmanuel270402@gmail.com>
2024-07-02 20:52:32 -0400

    Fix Routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -43,6 +43,7 @@ function Routes(){
         <NavigationContainer>
             <Stack.Navigator screenOptions={ {headerShown: false,} }>
 
+            <Stack.Screen name="Cartelera" component={ Cartelera } options={ {animation: 'fade_from_bottom',} }/>
             <Stack.Screen name="Login" component={ Login } options={ {animation: 'fade'} }/>
             <Stack.Screen name="Register" component={ Register } options={ {animation: 'slide_from_right'} }/>
             <Stack.Screen name="MakePassword" component={ MakePassword } options={ {animation: 'slide_from_right'} }/>
@@ -55,7 +56,7 @@ function Routes(){
 
             <Stack.Screen name="Chat" component={ Chat } options={ {animation: 'fade_from_bottom'} }/>
 
-            <Stack.Screen name="Cartelera" component={ Cartelera } options={ {animation: 'fade_from_bottom',} }/>
+            
             <Stack.Screen name="EventDetails" component={ DetallesEvento } options={ {animation: 'slide_from_right',} }/>
             <Stack.Screen name="CarritoCompra" component={ CarritoCompra } options={ {animation: 'slide_from_right',} }/>
             
@@ -63,7 +64,7 @@ function Routes(){
 
             <Stack.Screen name="FormasPago" component={ FormasPago } options={ {animation: 'slide_from_right',} }/>
 
-            <Stack.Screen name="AgregarPago" component={ PagarBoletos } options={ {animation: 'slide_from_right',} }/>
+            <Stack.Screen name="PagarBoletos" component={ PagarBoletos } options={ {animation: 'slide_from_right',} }/>
 
             
             <Stack.Screen name="ForoNoticias" component={ ForoNoticias } options={ {animation: 'fade'} }/>
