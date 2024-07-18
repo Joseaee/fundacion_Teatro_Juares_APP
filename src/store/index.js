f9c699e0ac9b0e05f7f0903eb6916b76ae93dbcc
Hendherson Patiño <patinohendherson@gmail.com>
2024-07-17 21:36:07 -0400

    Se agrego el noticiasReducer en el archivo index de store

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,13 @@
 import {configureStore} from '@reduxjs/toolkit'
 import boleteriaReducer from './boleteria/slice';
+import noticiasReducer from './noticias/slice';
 import authReducer from './auth/slice.js'
-
 
 export const store = configureStore({
     reducer: {
         boleteria: boleteriaReducer,
-        auth: authReducer
+        auth: authReducer,
+        noticias: noticiasReducer,
     },
 });
+
