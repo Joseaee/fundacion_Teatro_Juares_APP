25c0398274e0c578a9dc5f6fc4eedcfaa998b276
Hendherson Patiño <patinohendherson@gmail.com>
2024-07-17 21:33:29 -0400

    Se agrego el componente useState en el componente Banner

--- src/components/Banner/index.jsx
+++ src/components/Banner/index.jsx
@@ -1,5 +1,5 @@
 import {Text, ImageBackground, View, StyleSheet} from 'react-native'
-
+import React, { useState } from 'react';
 import Navbar from '../navbar'
 
 export default function Banner({children, image, goBack, darkOverlay = false, icons = {
@@ -7,32 +7,33 @@ export default function Banner({children, image, goBack, darkOverlay = false, ic
   bell: true,
   user: true
 }}){
+  
     return (
         <View style={{height: 160}}>
-              <ImageBackground
-                source={image} 
-                style={[styles.backgroundImage]}
-              >
-                {(darkOverlay) ?  <View style={styles.darkOverlay}/> : null}
-                <View style={{flexDirection: 'row'}}>
-                <Navbar title='' loggedIn={true} transparent={true} back={goBack} icons={icons}/>
-                </View>
-                <View style={{flexDirection: 'row'}}>
-                    {children}
-                </View>
-              </ImageBackground>
+          <ImageBackground
+            source={image} 
+            style={[styles.backgroundImage]}
+          >
+            {(darkOverlay) ?  <View style={styles.darkOverlay}/> : null}
+            <View style={{flexDirection: 'row'}}>
+            <Navbar title='' loggedIn={true} transparent={true} back={goBack} icons={icons}/>
             </View>
+            <View style={{flexDirection: 'row'}}>
+              {children}
+            </View>
+          </ImageBackground>
+        </View>
     )
 }
 
 const styles = StyleSheet.create({
-      backgroundImage: {
-        flex: 1,
-        resizeMode: 'cover',
-        alignItems: 'center',
-      },
-      darkOverlay: {
-        ...StyleSheet.absoluteFillObject,
-        backgroundColor: 'rgba(0,0,0,0.4)'
+    backgroundImage: {
+      flex: 1,
+      resizeMode: 'cover',
+      alignItems: 'center',
+    },
+    darkOverlay: {
+      ...StyleSheet.absoluteFillObject,
+      backgroundColor: 'rgba(0,0,0,0.4)'
     },
 })
