0cc9c92bf39b802a4d2c23602391b1d460f02b63
Joseaee <joseescalona1505@gmail.com>
2024-07-18 20:15:11 -0400

    ip jose

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,3 +1,7 @@
+<<<<<<< Updated upstream
 const API_URL = 'http://192.168.1.110/teatro_juares/'
+=======
+const API_URL = 'http://192.168.1.106/xampp/teatro_juares/'
+>>>>>>> Stashed changes
 
 export {API_URL}
