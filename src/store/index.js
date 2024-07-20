6baf177eef086045372d853071d5d4253347e26a
Hendherson Patiño <patinohendherson@gmail.com>
2024-07-19 22:59:37 -0400

    agg store

--- src/store/index.js
+++ src/store/index.js
@@ -1,24 +1,17 @@
 import {configureStore} from '@reduxjs/toolkit'
-import boleteriaReducer from './boleteria/slice';
-<<<<<<< HEAD
-import noticiasReducer from './noticias/slice';
-import authReducer from './auth/slice.js'
-=======
+import boleteriaReducer from './boleteria/slice'
+import noticiasReducer from './noticias/slice'
 import useReducer from './user/slice'
 import authReducer from './auth/slice.js'
-import profileSlice  from './profile/slice.js';
->>>>>>> c48d7b5b4c49b4ef7be0054c1a0a4575b5b58909
+import profileSlice  from './profile/slice.js'
 
 export const store = configureStore({
     reducer: {
         boleteria: boleteriaReducer,
         auth: authReducer,
-<<<<<<< HEAD
         noticias: noticiasReducer,
-=======
         user: useReducer,
         profile: profileSlice
->>>>>>> c48d7b5b4c49b4ef7be0054c1a0a4575b5b58909
     },
 });
 
